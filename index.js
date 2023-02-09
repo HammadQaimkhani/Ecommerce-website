a3bb44e69a173c4b1471177924ad8afc80d5000e
HAMMAD QAIMKHNAI <62787660+HammadQaimkhani@users.noreply.github.com>
2023-02-09 13:24:06 +0500

    kch nhi yr bs error hi diky hy

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-  async function fetchProducts(url) {
-    let data = await fetch(url);
-    let response = await data.json();
-    console.log(response);
-  }
-  fetchProducts("https://api.escuelajs.co/api/v1/products");
-});
+const icon = document.getElementById("svg-icon");
+const clickMe = () => {
+  icon.attributes.fill.nodeValue = "currentColor";
+};
